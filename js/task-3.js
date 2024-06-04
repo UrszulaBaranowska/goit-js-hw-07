82e442fbc836ba972a25556afa899df7a4186b74
UrszulaBaranowska <114485005+UrszulaBaranowska@users.noreply.github.com>
2024-06-04 22:23:12 +0200

    Task 3 ready

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,9 @@
-const textInput = document.querySelector(".text");
-const output = document.querySelector(".output");
-
-textInput.addEventListener("name-input", (event) => {
-  output.textContent = event.currentTarget.value;
+document.getElementById("name-input").addEventListener("input", function () {
+  const nameInput = document.getElementById("name-input").value.trim();
+  const nameOutput = document.getElementById("name-output");
+  if (nameOutput === " ") {
+    nameOutput.textContent = "Anonymous";
+  } else {
+    nameOutput.textContent = nameInput ? nameInput : "Anonymous";
+  }
 });
