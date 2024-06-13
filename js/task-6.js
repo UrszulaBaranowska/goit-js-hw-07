d086864ae26755857fbf3b87550be4607f09d126
UrszulaBaranowska <114485005+UrszulaBaranowska@users.noreply.github.com>
2024-06-13 12:22:51 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,44 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+function createBoxes(amount) {
+  const boxesContainer = document.getElementById("boxes");
+  const fragment = document.createDocumentFragment();
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    fragment.appendChild(box);
+    size += 10;
+  }
+
+  boxesContainer.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  const boxesContainer = document.getElementById("boxes");
+  boxesContainer.innerHTML = "";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const createButton = document.querySelector("[data-create]");
+  const destroyButton = document.querySelector("[data-destroy]");
+  const input = document.querySelector("#controls input");
+
+  createButton.addEventListener("click", () => {
+    const amount = parseInt(input.value);
+
+    if (amount >= 1 && amount <= 100) {
+      destroyBoxes();
+      createBoxes(amount);
+      input.value = "";
+    }
+  });
+
+  destroyButton.addEventListener("click", () => {
+    destroyBoxes();
+  });
+});
